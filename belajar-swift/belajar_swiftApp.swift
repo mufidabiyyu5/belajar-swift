2cd3b23351e02f81cd2d24bf0408e1b1f213f4d7
Mochmad Mufid Abiyyu <mochamad.mufid@prisma-ads.com>
2024-02-07 14:30:53 +0700

    Initial Commit

---
+++ belajar-swift/belajar_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  belajar_swiftApp.swift
+//  belajar-swift
+//
+//  Created by Prisma-Mufid on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct belajar_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
